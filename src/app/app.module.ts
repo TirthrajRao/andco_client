22c34e3b084df89d9a9df4b7240b0b1eb33ddadc
vivekbharda <vivekkbharda@gmail.com>
2020-02-13 13:09:13 +0530

    new project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DisplayHomeComponent } from './display-home/display-home.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    DisplayHomeComponent,
+    MainMenuComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-home/display-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-home',
+  templateUrl: './display-home.component.html',
+  styleUrls: ['./display-home.component.css']
+})
+export class DisplayHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
