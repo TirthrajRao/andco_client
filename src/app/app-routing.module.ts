41fb983e8d0e8361f1b3dd097dd893118a13a2a4
swati chauhan <chauhan.swati104@gmail.com>
2020-02-13 15:18:15 +0530

    login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{DisplayHomeComponent} from './display-home/display-home.component';
+import{SignUpComponent} from './sign-up/sign-up.component';
+import{LoginComponent} from './login/login.component'
+  import { from } from 'rxjs';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: 'display-page',
+      component: DisplayHomeComponent
+    },
+    {
+      path: 'signup',
+      component: SignUpComponent
+    },
+    {
+      path: 'login',
+      component: LoginComponent
+    }
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
