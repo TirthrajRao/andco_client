491b542a9af126414300a5eb5cca3b7d71c8eef7
vivekbharda <vivekkbharda@gmail.com>
2020-02-15 02:33:40 +0530

    changes in social login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
 import { AuthInterceptor } from './services/auth.interceptor';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -11,6 +13,25 @@ import { DisplayHomeComponent } from './display-home/display-home.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginService } from './services/login.service';
+
+
+/**
+ * Key For login with google and facebook 
+ */
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("261246561965-ikogp2hbouqaj7l9deflfr176lqa3p3u.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("350939005533804")
+  }
+]);
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -27,14 +48,20 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }
+    },
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    },
+    LoginService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -27,19 +27,11 @@ export class EventService {
     formdata.append('eventType', body.eventType);
     formdata.append('hashTag', body.hashTag);
     formdata.append('background', body.background)
-    // formdata.append('deadlineDate', body.deadlineDate);
-    // formdata.append('isPublic', body.isPublic);
-    // formdata.append('isLogistics', body.isLogistics);
     if (files.length) {
       for (let i = 0; i < files.length; i++) {
         formdata.append("profile", files[i]);
       }
     }
-    // if (themeFiles.length) {
-    //   for (let i = 0; i < themeFiles.length; i++) {
-    //     formdata.append("background", themeFiles[i]);
-    //   }
-    // }
     return this.http.post(config.baseApiUrl + "/event", formdata);
   }
 
