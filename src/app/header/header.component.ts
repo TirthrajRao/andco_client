1372f46ee7c2305322aaf29846e3e70d3bb78276
vivekbharda <vivekkbharda@gmail.com>
2020-02-17 20:28:44 +0530

    header content display changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd, RouterEvent, RoutesRecognized } from '@angular/router';
 // import { filter } from 'rxjs/operators';
 import { filter, pairwise } from 'rxjs/operators';
@@ -15,38 +15,15 @@ import { filter, pairwise } from 'rxjs/operators';
 })
 export class HeaderComponent implements OnInit {
   userName = JSON.parse(sessionStorage.getItem('userName'));
+  index = 0
   returnUrl: string;
   isDisplay = false
-  previousUrl: String
+  currentUrl: String
   constructor(
     private route: ActivatedRoute,
-    public router: Router
-
-  ) {
-    // router.events.pipe(
-    //   filter((event) => event instanceof NavigationEnd)
-    // ).subscribe(x => console.log("router name ============",x))
-
-    // router.events.forEach((singleEvent) => {
-    //   console.log("page name========", singleEvent)
-    // })
-
-    // this.router.events
-    //   .filter(e => e instanceof RoutesRecognized)
-    //   .pairwise()
-    //   .subscribe((event: any[]) => {
-    //     console.log("another page url name======", event[0].urlAfterRedirects)
-    //   })
-
-
-
-
-    // router.events
-    //   .filter(event => event instanceof NavigationEnd)
-    //   .subscribe(e => {
-    //     console.log("route name of another page", e)
-    //   })
-  }
+    public router: Router,
+    public _change: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     // console.log("login user name in heaedr", this.userName)
@@ -57,17 +34,31 @@ export class HeaderComponent implements OnInit {
       .pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
       .subscribe((events: RoutesRecognized[]) => {
         console.log("event of url find", events)
-        // this.previousUrl = events[0].urlAfterRedirects
-        if (events[1].urlAfterRedirects == '/menu') {
-          console.log("to ama ave")
-          this.isDisplay = true
-        } else {
-          this.isDisplay = false
-          console.log("ama ave baki")
-        }
-        console.log('previous url', events[0].urlAfterRedirects);
+        // console.log("index of page", this.index)
+        this.currentUrl = events[1].urlAfterRedirects
+        //     if (events[1].urlAfterRedirects == '/menu') {
+        //       console.log("to ama ave")
+        //       this.isDisplay = false
+        //     } else if (events[1].urlAfterRedirects != '/menu') {
+        //       console.log("added in else part", this.isDisplay)
+        //       this.displayData()
+        //     }
+        //     console.log('previous url', events[0].urlAfterRedirects);
+        // this._change.detectChanges()
         console.log('current url', events[1].urlAfterRedirects);
       });
+    this.currentUrl = this.router.url
+    // if (loadEvent != '/menu') {
+    //   this.isDisplay = true
+    // } else {
+    //   this.isDisplay = false
+    // }
+    console.log("whne page is load display route", this.currentUrl)
   }
+
+  // displayData() {
+  //   console.log("function ma ave che ke nai")
+  //   this.isDisplay == true
+  // }
 
 }
