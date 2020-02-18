e2c5bea44356879b369df3535c19b1a006fa1af0
swati chauhan <chauhan.swati104@gmail.com>
2020-02-18 16:18:36 +0530

    loader completed

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -7,7 +7,7 @@ declare var $: any;
 declare var global: any;
 
 // (window as any).global = window;
-(window as any).global.Buffer = (window as any).global.Buffer || require('buffer').Buffer;
+// (window as any).global.Buffer = (window as any).global.Buffer || require('buffer').Buffer;
 
 
 @Component({
