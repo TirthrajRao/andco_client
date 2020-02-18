50a73a8a8de3d9b791110ae00c010101e2120c14
vivekbharda <vivekkbharda@gmail.com>
2020-02-18 13:10:07 +0530

    datePicker for activity in progress

--- src/app/createEvent/event-activity/event-activity.component.ts
+++ src/app/createEvent/event-activity/event-activity.component.ts
@@ -10,11 +10,6 @@ export class EventActivityComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $('.wrapper').on('click', '.remove', function() {
-      $('.remove').closest('.wrapper').find('.element').not(':first').last().remove();
-    });
-    $('.wrapper').on('click', '.clone', function() {
-      $('.clone').closest('.wrapper').find('.element').first().clone().appendTo('.results');
-    });
+
   }
 }
