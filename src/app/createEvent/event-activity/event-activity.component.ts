4593cbe857f93e3995d9fbb8e165178f429f4cb0
swati chauhan <chauhan.swati104@gmail.com>
2020-02-18 12:25:59 +0530

    event-activity design completed

--- src/app/createEvent/event-activity/event-activity.component.ts
+++ src/app/createEvent/event-activity/event-activity.component.ts
@@ -14,7 +14,7 @@ export class EventActivityComponent implements OnInit {
       $('.remove').closest('.wrapper').find('.element').not(':first').last().remove();
     });
     $('.wrapper').on('click', '.clone', function() {
-      $('.clone').closest('.wrapper').find('.element').first().clone().appendTo('.results');
+      $('.clone').closest('.wrapper').find('.element').first().clone().appendTo('.inner-box');
     });
   }
 }
