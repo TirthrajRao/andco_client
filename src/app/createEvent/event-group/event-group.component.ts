f92fab77027beecd1bbb712738f16cafab452fe9
swati chauhan <chauhan.swati104@gmail.com>
2020-02-20 18:00:22 +0530

    for pull

--- src/app/createEvent/event-group/event-group.component.ts
+++ src/app/createEvent/event-group/event-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-event-group',
@@ -10,6 +11,26 @@ export class EventGroupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      // event main slider start
+      $('.event-slider').slick({
+        infinite: false,
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: false,
+
+    });
+    // event main slider end
+    //group slider start
+    $('.group-slider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: false,
+        
+    });
+    //group slider end
   }
 
 }
