14e1d1681697125cd2a76696134c13e86b15d482
vivekbharda <vivekkbharda@gmail.com>
2020-03-09 11:40:07 +0530

    display item in cart in progress

--- src/app/guest/guest-item-total/guest-item-total.component.ts
+++ src/app/guest/guest-item-total/guest-item-total.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-guest-item-total',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guest-item-total.component.css']
 })
 export class GuestItemTotalComponent implements OnInit {
-
+  @Input('totalItemList') displayTotalItem
+  totlaItem = []
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("display item in its page", changes.displayTotalItem.currentValue);
+    this.totlaItem = _.filter(changes.displayTotalItem.currentValue, function (x) {
+      console.log("single item ", x)
+      return x.activityName = x.activityName
+    })
+    console.log("final items list after filter", this.totlaItem);
+
   }
 
 }
