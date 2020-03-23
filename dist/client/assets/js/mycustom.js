47a45e825055e8b9b8bfd48362e82d64d6d0e530
vivekbharda <vivekkbharda@gmail.com>
2020-03-23 11:17:03 +0530

    remove dist file

---
+++ dist/client/assets/js/mycustom.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//     //tooltip js start
+  
+//     $(".tooltip-class").hover(function () {
+//         $(this).attr("tooltip-data", $(this).attr("title"));
+//         $(this).removeAttr("title");
+//     }, function () {
+//         $(this).attr("title", $(this).attr("tooltip-data"));
+//         $(this).removeAttr("tooltip-data");
+//     });
+//     //tooltip js end
+// });
