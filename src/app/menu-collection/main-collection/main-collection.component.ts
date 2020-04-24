c9c118065e6c6f288250f4954daf18cf632e0e39
vivekbharda <vivekkbharda@gmail.com>
2020-04-24 19:43:07 +0530

    display collection in main menu

--- src/app/menu-collection/main-collection/main-collection.component.ts
+++ src/app/menu-collection/main-collection/main-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event.service';
 declare var $;
 
 @Component({
@@ -9,13 +10,26 @@ declare var $;
 export class MainCollectionComponent implements OnInit {
   $slideContainter;
   $slider;
-
-  constructor() { }
+  isDisplay = false
+  listOfEvent = []
+  selectedIndex
+  navTabs = ["Total", "Guests"]
+  selectedActiveTab
+  totalOfEvent
+  totalCollection
+  indexOfPage
+  constructor(
+    public eventService: EventService
+  ) { }
 
   ngOnInit() {
-this.initEventSlider();
+    this.getTotalEvent()
+
+    this.initEventSlider();
   }
-initEventSlider() {
+
+
+  initEventSlider() {
     setTimeout(() => {
       this.$slideContainter = $('.myEvent-slider')
       this.$slider = this.$slideContainter.not('.slick-initialized').slick({
@@ -50,4 +64,34 @@ initEventSlider() {
   }
 
 
+
+  getTotalEvent() {
+    this.eventService.getLoginUserEvent().subscribe((response: any) => {
+      console.log("total event of login user", response);
+      this.listOfEvent = response.data
+    }, error => {
+      console.log("error while get event", error);
+
+    })
+  }
+
+  getCollection(event, index) {
+    this.selectedIndex = index
+    console.log("event id", event._id);
+    this.eventService.getEventCollection(event._id).subscribe((response: any) => {
+      console.log("response of collection", response);
+      this.isDisplay = true
+      this.totalOfEvent = response.data.eventTotal
+      this.totalCollection = response.data
+      this.selectedActiveTab = 0
+      this.indexOfPage = 0
+    }, error => {
+      console.log("error while get collections", error);
+
+    })
+  }
+
+  selectedTab(i) {
+    this.selectedActiveTab = i
+  }
 }
