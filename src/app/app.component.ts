01c0127b053ff0438f5611dba1efbc9aac1aa984
vivekbharda <vivekkbharda@gmail.com>
2020-05-25 15:41:31 +0530

    changes in images in guest side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,14 @@ export class AppComponent {
   title = 'client';
 
   ngOnInit() {
-    console.log("get the width of screen", screen.width);
-    console.log("get the height of screen ", screen.height);
-    if (screen.width < 575) {
-      console.log("this is perfect for mobile size");
-      sessionStorage.setItem('isMobile', 'true')
-    } else {
-      sessionStorage.setItem('isMobile', 'false')
-    }
+    // console.log("get the width of screen", screen.width);
+    // console.log("get the height of screen ", screen.height);
+    // if (screen.width < 575) {
+    //   console.log("this is perfect for mobile size");
+    //   sessionStorage.setItem('isMobile', 'true')
+    // } else {
+    //   sessionStorage.setItem('isMobile', 'false')
+    // }
 
 
     // console.log(moment.tz.names());  // Get list of all available time zone names
